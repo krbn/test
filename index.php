1014153fe0033bd5a8083b0db4d1c66bb578802f
inside <inside@inside.com>
2015-05-28 06:52:00 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="script.js">var name = 'john';</script>
+</head>
+<body>
+	<?php
+		echo '<h2>HELLO WORLD!<h2>';
+		echo 'new line';
+		echo 'another line';
+	?>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,1 @@
+var foo = function(){ alert('hello, world!'); };
